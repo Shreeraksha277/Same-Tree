c170f9c0f62c5a30e6cf861a1f9c2b6bbb9fe3e4
Shreeraksha Kulal <shreeraksha8147@gmail.com>
2026-05-01 19:14:29 +0530

    Create sol.java

---
+++ sol.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        // If both nodes are null, trees match at this branch
+        if (p == null && q == null) {
+            return true;
+        }
+
+        // If one is null and the other isn't, trees differ
+        if (p == null || q == null) {
+            return false;
+        }
+
+        // If values differ, trees are not the same
+        if (p.val != q.val) {
+            return false;
+        }
+
+        // Recursively compare left and right subtrees
+        return isSameTree(p.left, q.left) &&
+               isSameTree(p.right, q.right);
+    }
+}
